fe1e9c383218dddec675a79ae8d899f3e69d3fe2
Aidos Batyrkulov <aidosbatyrkulov@gmail.com>
2020-09-02 16:49:25 +0600

    Updates

---
+++ precache-manifest.8dbf85799091041b29665f04687ab2e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e07d1f854516805226a91bd22b2c3b",
+    "url": "/todo-redux/index.html"
+  },
+  {
+    "revision": "5c126a72bb13aed29a7d",
+    "url": "/todo-redux/static/css/main.de7da6eb.chunk.css"
+  },
+  {
+    "revision": "9c87b8f26dda6c021824",
+    "url": "/todo-redux/static/js/2.95d066d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-redux/static/js/2.95d066d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c126a72bb13aed29a7d",
+    "url": "/todo-redux/static/js/main.bb9c1ece.chunk.js"
+  },
+  {
+    "revision": "d8e8d408843e3de49768",
+    "url": "/todo-redux/static/js/runtime-main.5b402628.js"
+  }
+]);
